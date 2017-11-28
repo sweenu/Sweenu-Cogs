cbe4037ea1c8a41e00f26a9954fb4637af0a0f36
Sweenu <brunoinec@gmail.com>
2017-11-28 22:17:06 +0100

    first commit

---
+++ league/league.py
@@ -0,0 +1,95 @@
+import aiohttp
+from discord.ext import commands
+
+api_key = 'RGAPI-1bc7569a-ac39-4570-9d0c-fd4775c12c29'
+
+
+class League:
+    """Cog that can fetch different information about League of Legends."""
+
+    def __init__(self, bot, platform):
+        self.bot = bot
+        self.url = "https://{}.api.riotgames.com".format(platform)
+
+    async def _fetch_url(self, path) -> dict:
+        """Fetch info from League of Legends api."""
+        url = '{}/{}?api_key={}'.format(self.url, path, api_key)
+        async with aiohttp.get(url) as r:
+            return await r.json()
+
+    def _get_summoner(self, summonerName):
+        path = '/lol/summoner/v3/summoners/by-name/'
+        return self._fetch_url(path + summonerName)
+
+    def _get_activeGame(self, summonerId):
+        path = '/lol/spectator/v3/active-games/by-summoner/'
+        return self._fetch_url(path + summonerId)
+
+    def _get_position(self, summonerId):
+        path = '/lol/league/v3/positions/by-summoner/'
+        return self._fetch_url(path + summonerId)
+
+    def _get_champion(self, championId):
+        path = '/lol/static-data/v3/champions/'
+        return self._fetch_url(path + championId)
+
+    def _get_maps(self):
+        path = '/lol/static-data/v3/maps'
+        return self._fetch_url(path)
+
+    @commands.command()
+    async def gameinfo(self, summonerName):
+        """Fetch info about the currently active game of a summoner."""
+        summonerId = self._get_summoner(summonerName)['id']
+        active_game = self._get_activeGame(summonerId)
+        maps = self._get_maps()
+        for m in maps['data']:
+            if m['mapId'] == active_game['mapId']:
+                map_name = m['mapName']
+                break
+
+        # list of participants in the form:
+        # (Summoner name, Champion, Rank solo, Rank flex)
+        team1 = []
+        team2 = []
+        id_team1 = active_game['participants'][0]['teamId']
+        for participant in active_game['participants']:
+            league = self._get_position(participant['summonerId'])
+            champion = self._get_champion(participant['championId'])['name']
+            for queue in league:
+                if queue['queueType'] == 'RANKED_SOLO_5x5':
+                    rank_solo = "{} {}".format(queue['tier'], queue['rank'])
+                elif queue['queueType'] == 'RANKED_FLEX_SR':
+                    rank_flex = "{} {}".format(queue['tier'], queue['rank'])
+
+            info = participant['summonerName'], champion, rank_solo, rank_flex
+            if participant['teamId'] == id_team1:
+                team1.append(info)
+            else:
+                team2.append(info)
+
+        table_format = '{0:16} {1:12} {2:14} {3:14}'
+        unformatted_header = table_format + ' | ' + table_format + '\n'
+        header = unformatted_header.format('Name', 'Champion', 'Solo', 'Flex')
+        header += '-' * 120 + '\n'
+
+        player_list = ''
+        for i in range(len(team1)):
+            player_list += table_format.format(*team1[i]) + ' | '
+            player_list += table_format.format(*team2[i]) + ' \n '
+
+        final_output = ('```'
+                        '**{map_name}**\n'
+                        '*{game_mode}*\n\n'
+                        '{header}'
+                        '{player_list}'
+                        '```').format(map_name=map_name,
+                                      game_mode=active_game['gameMode'],
+                                      header=header,
+                                      player_list=player_list)
+
+        await self.bot.say(final_output)
+
+
+def setup(bot):
+    bot.add_cog(League(bot, 'euw1'))
